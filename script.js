3a1df19caf09207de06b161dd105210e0eeea0a6
Hari Om Jee <hariomjee2@gmail.com>
2017-12-15 14:51:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,26 @@
 				controller  : 'contactController'
 			});
 	});
+	scotchApp.run($run);
+	// Safely instantiate dataLayer
+	  var dataLayer = window.dataLayer = window.dataLayer || [];
+
+	  $run.$inject = ['$rootScope', '$location'];
+
+	  function $run($rootScope, $location) {
+
+	    $rootScope.$on('$routeChangeSuccess', function() {
+
+	      dataLayer.push({
+		event: 'ngRouteChange',
+		attributes: {
+		  route: $location.path()
+		}
+	      });
+
+	    });
+
+	  }
 
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
